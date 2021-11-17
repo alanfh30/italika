49eef9a3cd2ecc327176ed9744ccd06376f23de8
Alan Flores <alanfh29132@gmail.com>
2021-11-17 10:18:17 -0600

    cotizaciones por persona

--- src/main/java/com/example/italika/repositories/cotizacionRepository.java
+++ src/main/java/com/example/italika/repositories/cotizacionRepository.java
@@ -4,11 +4,15 @@ import com.example.italika.models.cotizacionModel;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.ArrayList;
+
+
 
 @Repository
 
 public interface cotizacionRepository extends CrudRepository<cotizacionModel, Long>{
-
-    
-
+    @Query("select c from  cotizacionModel c where c.id_usuario = :id_usuario")
+    ArrayList<cotizacionModel> findCotizaciones(@Param("id_usuario") Long id_usuario);
 }
--- src/main/java/com/example/italika/services/cotizacionService.java
+++ src/main/java/com/example/italika/services/cotizacionService.java
@@ -7,6 +7,7 @@ import com.example.italika.repositories.cotizacionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
@@ -14,11 +15,25 @@ public class cotizacionService {
     @Autowired
     cotizacionRepository cotizacionRepository;
 
+
+    @Transactional(readOnly = true)
     public ArrayList<cotizacionModel> obtenerCotizacion(){
         return (ArrayList<cotizacionModel>) cotizacionRepository.findAll();
     }
 
+    @Transactional(readOnly = true)
     public cotizacionModel guardarCotizacion(cotizacionModel usuario){
         return cotizacionRepository.save(usuario);
     }
+
+    @Transactional(readOnly = true)
+    public  ArrayList<cotizacionModel> encontrarCotizacionesByIds(cotizacionModel cotizacionModel){
+        return cotizacionRepository.findCotizaciones(cotizacionModel.getId_usuario());
+    }
+
+    @Transactional(readOnly = true)
+    public cotizacionModel findbyid(cotizacionModel cotizacionModel){
+        return cotizacionRepository.findById(cotizacionModel.getId()).orElse(null);
+    }
+
 }
